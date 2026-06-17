65d6de41706d48251f3c1bc1629cff509ee20836
St-Tomas <tomastrevensky@gmail.com>
2026-06-17 02:24:35 -0300

    Agrege Dtos de la rama models

---
+++ Dtos/PersonaMostrarDto.cs
@@ -0,0 +1,9 @@
+public class PersonaMostrarDTO
+{
+    public int Id { get; set; }
+
+    public int Dni { get; set; }
+
+    public string Nombre { get; set; }
+    public string Email {get; set;}
+}
